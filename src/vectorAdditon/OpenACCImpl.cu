6234520a1a0cdfb21644cc56d960181ca8f73965
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2024-08-23 16:16:25 +0200

    OpenACCImpl

---
+++ src/vectorAdditon/OpenACCImpl.cu
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <benchmark/benchmark.h>
+#include <openacc.h>
+#include "VectorAddition.h"
+
+template <typename FloatType>
+std::vector<FloatType> VectorAddition<FloatType>::operator()() {
+    const size_t size = _inA.size();
+    FloatType *a = _inA.data();
+    FloatType *b = _inB.data();
+    FloatType *c = _outC.data();
+#pragma acc parallel loop copyin(a[0 : size], b[0 : size]) copyout(c[0 : size])
+    for (size_t i = 0; i < size; ++i) {
+        c[i] = a[i] + b[i];
+    }
+    checkValidity();
+    return _outC;
+}
+
+template std::vector<float> VectorAddition<float>::operator()();
+BENCHMARK(VectorAddition<float>::vectorAdditionBenchmark)
+    ->Name("VecAdd-OpenACC-Float")
+    ->RangeMultiplier(10)
+    ->Range(1e3, 1e8)
+    ->Complexity();
+
+int main(int argc, char **argv) {
+    int num_devices = acc_get_num_devices(acc_device_default);
+    printf("Number of available devices %d\n", num_devices);
+
+#pragma acc parallel
+    {
+        int device_num = acc_get_device_num(acc_device_default);
+        printf("Running on device number %d\n", device_num);
+    }
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+    benchmark::Shutdown();
+}
