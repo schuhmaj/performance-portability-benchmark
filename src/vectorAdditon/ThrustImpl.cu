3f7c77e5798cd2f7ec3fb065a464172100cf5421
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2024-08-23 11:19:25 +0200

    update thrust

--- src/vectorAdditon/ThrustImpl.cu
+++ src/vectorAdditon/ThrustImpl.cu
@@ -3,6 +3,7 @@
 #include "thrust/transform.h"
 #include "thrust/device_vector.h"
 #include "thrust/host_vector.h"
+#include "thrust/execution_policy.h"
 
 
 template<typename FloatType>
@@ -14,7 +15,7 @@ std::vector<FloatType> VectorAddition<FloatType>::operator()() {
     thrust::device_vector<FloatType> deviceC(size);
 
     // Step 2: Addition of vector
-    thrust::transform(deviceA.begin(), deviceA.end(), deviceB.begin(), deviceC.begin(), thrust::plus<float>());
+    thrust::transform(thrust::device, deviceA.begin(), deviceA.end(), deviceB.begin(), deviceC.begin(), thrust::plus<float>());
 
     // Step 3: Copy result from device to host
     thrust::copy(deviceC.begin(), deviceC.end(), _outC.begin());
@@ -28,7 +29,7 @@ template std::vector<float> VectorAddition<float>::operator()();
 BENCHMARK(VectorAddition<float>::vectorAdditionBenchmark)->Name("VecAdd-Thrust-Float")->RangeMultiplier(10)->Range(1e3, 1e8)->Complexity();
 
 template std::vector<double> VectorAddition<double>::operator()();
-BENCHMARK(VectorAddition<double>::vectorAdditionBenchmark)->Name("VecAdd-Thrust-Double")->RangeMultiplier(10)->Range(1e3, 1e8)->Complexity();
+BENCHMARK(VectorAddition<double>::vectorAdditionBenchmark)->Name("VecAdd-Thrust-Double")->RangeMultiplier(10)->Range(1e3, 1e7)->Complexity();
 
 int main(int argc, char** argv) {
     benchmark::Initialize(&argc, argv);
