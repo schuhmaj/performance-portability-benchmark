a0916201a0858de89670e7a7cadd1d63ae3da7d5
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2024-08-27 16:32:31 +0200

    adapt color

--- scripts/plot_benchmark.py
+++ scripts/plot_benchmark.py
@@ -1,12 +1,12 @@
-import subprocess
-import json
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import argparse
-from loguru import logger
-
+import json
+import subprocess
 from pathlib import Path
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from loguru import logger
 
 
 def find_files(search_dir: Path, pattern: str) -> list[Path]:
@@ -29,13 +29,8 @@ def run_benchmarks(executable_targets: list[Path]) -> list[Path]:
             continue
         try:
             logger.info(f"Benchmarking {target} started!")
-            subprocess.run(
-                f"{target} --benchmark_out={output_file.name}",
-                shell=True,
-                check=True,
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL,
-            )
+            subprocess.run(f"{target} --benchmark_out={output_file.name}", shell=True, check=True,
+                stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, )
             logger.info(f"Benchmarking {target} finished!")
         except subprocess.CalledProcessError as e:
             logger.error(f"Failed to execute {target}: {e}")
@@ -51,50 +46,45 @@ def load_reports(report_files: list[Path]) -> pd.DataFrame:
         df = pd.DataFrame(json_data["benchmarks"])
         # Assuming df is your DataFrame
         # 1. Drop rows where `iterations` is NaN
-        df = df.dropna(subset=['iterations'])
+        df = df.dropna(subset=["iterations"])
 
         # 2. Select specific columns
-        df = df[['name', 'iterations', 'real_time', 'cpu_time', 'time_unit']]
+        df = df[["name", "iterations", "real_time", "cpu_time", "time_unit"]]
 
         # 3. Split the `name` column into `type`, `framework`, `precision`, and `size`
         # Assuming the format is consistent as 'VecAdd-Kokkos-Float/1000'
         # Separate by '/' first, then '-'
-        name_split = df['name'].str.split('/', expand=True)
-        details_split = name_split[0].str.split('-', expand=True)
+        name_split = df["name"].str.split("/", expand=True)
+        details_split = name_split[0].str.split("-", expand=True)
 
         # Create new columns from the splits
-        df['Problem'] = details_split[0]
-        df['Framework'] = details_split[1].str.lower()
-        df['Data Type'] = details_split[2].str.lower()
-        df['Problem Size'] = name_split[1].astype(int)
+        df["Problem"] = details_split[0]
+        df["Framework"] = details_split[1].str.lower()
+        df["Data Type"] = details_split[2].str.lower()
+        df["Problem Size"] = name_split[1].astype(int)
 
-        df.rename(
-            columns={
-                "real_time": "Runtime",
-                "cpu_time": "CPU Time",
-                "time_unit": "Time Unit",
-                "iterations": "Iterations",
-            },
-            inplace=True
-        )
+        df.rename(columns={"real_time": "Runtime", "cpu_time": "CPU Time", "time_unit": "Time Unit",
+            "iterations": "Iterations", }, inplace=True, )
 
         # Rearrange the columns as needed
-        df = df[['Problem', 'Framework', 'Data Type', 'Problem Size', 'Iterations', 'Runtime', 'CPU Time', 'Time Unit']]
+        df = df[
+            ["Problem", "Framework", "Data Type", "Problem Size", "Iterations", "Runtime", "CPU Time", "Time Unit", ]]
         data.append(df)
     logger.info(f"Loaded {len(data)} report json files")
     return pd.concat(data)
 
 
 def plot_benchmarks(df: pd.DataFrame, save_path: Path = Path("./results.pdf")) -> tuple[plt.Figure, plt.Axes]:
-    df = df[df['Data Type'] == 'float']
+    df = df[df["Data Type"] == "float"]
     fig, ax = plt.subplots(figsize=(10, 6))
-    sns.lineplot(x='Problem Size', y='Runtime', hue='Framework', hue_order=df["Framework"].unique().sort() ,  data=df, marker='o', ax=ax)
-    ax.set_xscale('log')
-    ax.set_yscale('log')
-    ax.set_ylabel('Runtime [ns]')
-    ax.set_xlabel('Problem Size [1]')
+    sns.lineplot(x="Problem Size", y="Runtime", hue="Framework", hue_order=df["Framework"].unique().sort(), data=df,
+        marker="o", ax=ax, palette="tab20", )
+    ax.set_xscale("log")
+    ax.set_yscale("log")
+    ax.set_ylabel("Runtime [ns]")
+    ax.set_xlabel("Problem Size [1]")
     ax.set_title(f'Runtime in nanoseconds vs. Problem Size $N$ for {df["Problem"].unique()[0]}')
-    ax.grid(True, which='both', linestyle='--', alpha=0.7)
+    ax.grid(True, which="both", linestyle="--", alpha=0.7)
     fig.tight_layout()
     fig.savefig(save_path, dpi=300)
     logger.info(f"Saved results to file {save_path}")
@@ -103,10 +93,12 @@ def plot_benchmarks(df: pd.DataFrame, save_path: Path = Path("./results.pdf")) -
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Benchmarking Command Line Interface")
-    parser.add_argument('--report', action='store_false', default=True, help='If set, the regex and path are used to directly search for the report files instead of benchmarking targets')
-    parser.add_argument('-p', '--path', type=Path, default=Path.cwd(), help="Path to search for files")
-    parser.add_argument('-r', '--regex', type=str, required=True, help="Regex Pattern for files to search")
-    parser.add_argument('-o', '--output', type=Path, default=Path("result.pdf"), help="Output file name for the plots")
+    parser.add_argument("--report", action="store_false", default=True,
+        help="If set, the regex and path are used to directly search for the report files instead of benchmarking targets", )
+    parser.add_argument("-p", "--path", type=Path, default=Path.cwd(), help="Path to search for files")
+    parser.add_argument("-r", "--regex", type=str, required=True, help="Regex Pattern for files to search", )
+    parser.add_argument("-o", "--output", type=Path, default=Path("result.pdf"),
+        help="Output file name for the plots", )
 
     args = parser.parse_args()
 
