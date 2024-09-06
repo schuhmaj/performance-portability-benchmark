209f7e89dc6fb0b5bfdc752a732341f6f0972b16
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2024-09-06 14:18:13 +0200

    adaptive cpp stdpar

--- src/vectorAdditon/NvhpcStdImpl.cpp
+++ src/vectorAdditon/NvhpcStdImpl.cpp
@@ -1,26 +1,20 @@
 #include <algorithm>
 #include <benchmark/benchmark.h>
-#ifdef __NVCOMPILER
 #include <execution>
-#endif
 #include "VectorAddition.h"
 
 
 template <typename FloatType>
 std::vector<FloatType> VectorAddition<FloatType>::operator()() {
-#ifdef __NVCOMPILER
     std::transform(std::execution::par_unseq, _inA.begin(), _inA.end(), _inB.begin(), _outC.begin(),
                    std::plus<FloatType>());
-#else
-    std::transform(_inA.begin(), _inA.end(), _inB.begin(), _outC.begin(), std::plus<FloatType>());
-#endif
     checkValidity();
     return _outC;
 }
 
 template std::vector<float> VectorAddition<float>::operator()();
 BENCHMARK(VectorAddition<float>::vectorAdditionBenchmark)
-    ->Name("VecAdd-CStd-Float")
+    ->Name("VecAdd-NvhpcCStd-Float")
     ->RangeMultiplier(10)
     ->Range(1e3, 1e8)
     ->Complexity();
