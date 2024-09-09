a3c266d9944a9063f6cf997402ca0c147b7bfb83
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2024-09-09 17:59:27 +0200

    revert changes

--- cmake/gbench.cmake
+++ cmake/gbench.cmake
@@ -1,25 +1,12 @@
-message(STATUS "Setting up Google Benchmark")
+include(FetchContent)
 
-# Specify the Google Benchmark version you want to use
-set(GOOGLEBENCH_VERSION 1.9.0)
+message(STATUS "Setting up googlebench")
 
-# Try to find an existing installation of Google Benchmark
-find_package(benchmark ${GOOGLEBENCH_VERSION})
+FetchContent_Declare(googlebench
+        GIT_REPOSITORY https://github.com/google/benchmark.git
+        GIT_TAG v1.8.4
+)
 
-if (benchmark_FOUND)
-        message(STATUS "Found existing Google Benchmark libraries: ${benchmark_DIR}")
-else ()
-        message(STATUS "Using Google Benchmark from GitHub release ${GOOGLEBENCH_VERSION}")
+option(BENCHMARK_ENABLE_TESTING "" OFF)
 
-        # Declare Google Benchmark to be fetched from GitHub
-        FetchContent_Declare(googlebench
-                GIT_REPOSITORY https://github.com/google/benchmark.git
-                GIT_TAG v1.9.0
-        )
-
-        # Optionally disable testing for Google Benchmark
-        option(BENCHMARK_ENABLE_TESTING "" OFF)
-
-        # Fetch the content and make it available
-        FetchContent_MakeAvailable(googlebenchmark)
-endif ()
+FetchContent_MakeAvailable(googlebench)
