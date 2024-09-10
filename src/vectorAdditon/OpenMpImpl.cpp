abfad37541e0876119a546fbc7003d3f7e74ddac
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2024-09-10 14:18:38 +0000

    OpenMP (how it should work on GPUs, but doesn't)

--- src/vectorAdditon/OpenMpImpl.cpp
+++ src/vectorAdditon/OpenMpImpl.cpp
@@ -26,22 +26,8 @@ BENCHMARK(VectorAddition<float>::vectorAdditionBenchmark)
     ->Complexity();
 
 int main(int argc, char **argv) {
-    int num_devices = omp_get_num_devices();
-    printf("Number of available devices %d\n", num_devices);
-
-#pragma omp target
-    {
-        if (omp_is_initial_device()) {
-            printf("Running on host\n");
-        }
-        else {
-            int nteams = omp_get_num_teams();
-            int nthreads = omp_get_num_threads();
-            printf("Running on device with %d teams in total and %d threads in each team\n", nteams, nthreads);
-        }
-    }
     // Deactivate Benchmrk
-    // benchmark::Initialize(&argc, argv);
-    // benchmark::RunSpecifiedBenchmarks();
-    // benchmark::Shutdown();
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+    benchmark::Shutdown();
 }
