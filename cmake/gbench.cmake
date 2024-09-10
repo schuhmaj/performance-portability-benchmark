5cd50e5f379c1cdbbb67ee3223599f5faa21ee1c
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2024-09-10 10:45:57 +0200

    update google benchmark setup

--- cmake/gbench.cmake
+++ cmake/gbench.cmake
@@ -1,12 +1,21 @@
 include(FetchContent)
 
-message(STATUS "Setting up googlebench")
+message(STATUS "Setting up Google Benchmark")
+set(GOOGLE_BENCHMARK_VERSION 1.9.0)
 
-FetchContent_Declare(googlebench
-        GIT_REPOSITORY https://github.com/google/benchmark.git
-        GIT_TAG v1.8.4
-)
+find_package(benchmark ${GOOGLE_BENCHMARK_VERSION} QUIET)
 
-option(BENCHMARK_ENABLE_TESTING "" OFF)
+if (${benchmark_FOUND})
+    message(STATUS "Found existing Google Benchmark: ${benchmark_DIR}")
+else ()
+    message(STATUS "Using Google Benchmark from GitHub Release ${GOOGLE_BENCHMARK_VERSION}")
 
-FetchContent_MakeAvailable(googlebench)
+    FetchContent_Declare(benchmark
+            GIT_REPOSITORY https://github.com/google/benchmark.git
+            GIT_TAG v${GOOGLE_BENCHMARK_VERSION}
+    )
+
+    set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "Disable benchmark testing" FORCE)
+
+    FetchContent_MakeAvailable(benchmark)
+endif ()
