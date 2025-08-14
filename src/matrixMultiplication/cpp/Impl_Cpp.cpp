8303677a6441f779d29a0d74ca0d95b303ee3ee6
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2025-08-14 14:57:07 +0200

    tile size

--- src/matrixMultiplication/cpp/Impl_Cpp.cpp
+++ src/matrixMultiplication/cpp/Impl_Cpp.cpp
@@ -5,10 +5,14 @@ namespace ppb {
     std::vector<FloatType> ImplCpp<FloatType>::operator()(const std::vector<FloatType> &a,
                                                                const std::vector<FloatType> &b, const MatrixMultiplicationConfig &config) {
         std::vector<FloatType> result(config.m * config.n, 0.0);
-        for (int i = 0; i < config.m; ++i) {
-            for (int entry = 0; entry < config.k; ++entry) {
-                for (int j = 0; j < config.n; ++j) {
-                    result[i * config.n + j] += a[i * config.k + entry] * b[entry * config.n + j];
+        constexpr int TILE_SIZE = 64;
+        for (int tile = 0; tile < config.k; tile += TILE_SIZE) {
+            const int endK = std::min(tile + TILE_SIZE, config.k);
+            for (int i = 0; i < config.m; ++i) {
+                for (int entry = tile; entry < endK; ++entry) {
+                    for (int j = 0; j < config.n; ++j) {
+                        result[i * config.n + j] += a[i * config.k + entry] * b[entry * config.n + j];
+                    }
                 }
             }
         }
