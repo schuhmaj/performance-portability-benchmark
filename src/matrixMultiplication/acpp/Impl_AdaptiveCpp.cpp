f853e98813690a8e29187ebac6eff0cb74010696
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2025-10-07 13:04:18 +0200

    split shared memory adaptive cpp impl into its own class

--- src/matrixMultiplication/acpp/Impl_AdaptiveCpp.cpp
+++ src/matrixMultiplication/acpp/Impl_AdaptiveCpp.cpp
@@ -40,53 +40,6 @@ std::pair<std::vector<FloatType>, double> ppb::ImplAdaptiveCpp<FloatType>::opera
             deviceResult[row + column * M] = sum;
         });
     });
-    // Using Work Groups and Shared Memory
-    // auto event = queue.submit([&](sycl::handler &h) {
-    //     constexpr size_t TM = 16;
-    //     constexpr size_t TN = 16;
-    //     constexpr size_t TK = 16;
-    //
-    //     sycl::range<2> local{TM, TN};
-    //     sycl::range<2> global{util::roundUp(M, local[0]), util::roundUp(N, local[1])};
-    //
-    //     sycl::local_accessor<FloatType, 2> shrA({TM, TK}, h);
-    //     sycl::local_accessor<FloatType, 2> shrB({TK, TN}, h);
-    //     h.parallel_for(
-    //         sycl::nd_range<2>{global, local},
-    //         [=](sycl::nd_item<2> it) {
-    //             const size_t local_row = it.get_local_id(0);
-    //             const size_t local_column = it.get_local_id(1);
-    //             const size_t row = it.get_global_id(0);
-    //             const size_t column = it.get_global_id(1);
-    //
-    //             if (row >= M || column >= N) {
-    //                 return;
-    //             }
-    //             FloatType sum = 0.0;
-    //             for (size_t k = 0; k < K; k += TK) {
-    //                 const size_t kA = k + local_column;
-    //                 if (row < M && kA < K) {
-    //                     shrA[local_row][local_column] = deviceA[row + kA * M];
-    //                 } else {
-    //                     shrA[local_row][local_column] = 0.0;
-    //                 }
-    //
-    //                 const size_t kB = k + local_row; // use local row to span TK
-    //                 if (kB < K && column < N) {
-    //                     shrB[local_row][local_column] = deviceB[kB + column * K];
-    //                 } else {
-    //                     shrB[local_row][local_column] = 0.0;
-    //                 }
-    //                 it.barrier(sycl::access::fence_space::local_space);
-    //
-    //                 for (size_t kk = 0; kk < TK; ++kk) {
-    //                     sum += shrA[local_row][kk] * shrB[kk][local_column];
-    //                 }
-    //                 it.barrier(sycl::access::fence_space::local_space);
-    //             }
-    //             deviceResult[row + column * M] = sum;
-    //         });
-    // });
     event.wait_and_throw();
     auto end = event.template get_profiling_info<sycl::info::event_profiling::command_end>();
     auto start = event.template get_profiling_info<sycl::info::event_profiling::command_start>();
--- src/matrixMultiplication/acpp/main.cpp
+++ src/matrixMultiplication/acpp/main.cpp
@@ -1,9 +1,19 @@
 #include "benchmark/benchmark.h"
 #include "Impl_AdaptiveCpp.h"
+#include "Impl_AdaptiveCppShr.h"
 #include "matrixMultiplication/MatrixMultiplication.h"
 
 BENCHMARK(ppb::MatrixMultiplication<ppb::ImplAdaptiveCpp<float>>::benchmark)
-    ->Name("MatrixMultiplication-Float-AdaptiveCpp")
+    ->Name("MatrixMultiplication-Float-AdaptiveCpp-Naive")
+    ->RangeMultiplier(2)
+    ->Range(32, 8192)
+#ifdef PPB_MEASURE_ONLY_KERNEL
+    ->UseManualTime()
+#endif
+    ->Complexity();
+
+BENCHMARK(ppb::MatrixMultiplication<ppb::ImplAdaptiveCppShr<float>>::benchmark)
+    ->Name("MatrixMultiplication-Float-AdaptiveCpp-SharedMemory")
     ->RangeMultiplier(2)
     ->Range(32, 8192)
 #ifdef PPB_MEASURE_ONLY_KERNEL
