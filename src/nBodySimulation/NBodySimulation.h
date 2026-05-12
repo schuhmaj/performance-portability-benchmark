e026f5652ee4f96cd9818a9cb7e1a5bc5192af1e
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2026-05-12 11:06:39 +0200

    make doc comments

--- src/nBodySimulation/NBodySimulation.h
+++ src/nBodySimulation/NBodySimulation.h
@@ -67,13 +67,12 @@ namespace ppb {
         std::array<FloatType, 3> boxMax{1000, 1000, 1000};
 
 
-
         /**
          * Flag to decide whether or not to use the kompute timestamps rather than the backend timestamps in vulkan
          */
         static constexpr bool use_kompute_timestamps{false};
 
-        /*
+        /**
          * Used for calculating the number of cells when using cell lists.
          * This is a magic number and should still be tested.
          * The influence of the Lennard-Jones-Kernel stays close to 0 at a distance of around 3. Therefore h should not be less than 3. 
@@ -81,7 +80,7 @@ namespace ppb {
          */
         static constexpr FloatType h{9.0};
 
-        /*
+        /**
          * Radius at which a particle should be added to the verlet lists.
          * This is a magic number and should still be tested. 
          * The influence of the Lennard-Jones-Kernel stays close to 0 at a distance of around 3. 
@@ -90,14 +89,14 @@ namespace ppb {
          */
         static constexpr FloatType influenceRadius{4.0};
 
-        /*
+        /**
          * size of workgroups. 
          * NOTE: if updated it should also be updated in the relevant shader files.
          * This is a magic number and should still be tested.
          */
         static constexpr uint TILE_SIZE{256};
 
-        /*
+        /**
          * compute neighbor search every interval_neighbor_search iterations.
          * example: if interval_neighbor_search = 1 then compute neighbor search every frame,
          *    if interval_neighbor_search = 10 then compute neighbor search every 10th frame.
