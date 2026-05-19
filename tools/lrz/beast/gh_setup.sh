ec0b42512c6248ebf9c20ae75faaab799d250b5c
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2026-05-19 16:18:49 +0200

    add setup

---
+++ tools/lrz/beast/gh_setup.sh
@@ -0,0 +1,3 @@
+# LRZ Beast Grace-Hopper Setup
+module load nvhpc/25.3 llvm/19.1.2-gcc-13.2.0-eoqwplw cmake/3.30.5-gcc-13.2.0-qjohagx ninja/1.12.1-gcc-13.2.0-3zszrzs
+module load AdaptiveCpp/25.10.0 google-benchmark/1.9.4 googletest/1.17.0 kokkos/4.7.02 spdlog/1.16.0 VulkanSDK/1.4.350.0
