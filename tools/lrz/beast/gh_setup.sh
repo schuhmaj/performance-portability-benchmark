f0fb7fd6fa1861f6d39e9eb2c2efe90ac6f6e184
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2026-05-20 15:21:51 +0200

    slang matmul draft

--- tools/lrz/beast/gh_setup.sh
+++ tools/lrz/beast/gh_setup.sh
@@ -1,3 +1,2 @@
 # LRZ Beast Grace-Hopper Setup
-module load nvhpc/25.3 llvm/19.1.2-gcc-13.2.0-eoqwplw cmake/3.30.5-gcc-13.2.0-qjohagx ninja/1.12.1-gcc-13.2.0-3zszrzs
-module load AdaptiveCpp/25.10.0 google-benchmark/1.9.4 googletest/1.17.0 kokkos/4.7.02 spdlog/1.16.0 VulkanSDK/1.4.350.0
+module load cuda/12.8 llvm/20.1.8 cmake/3.30.5-gcc-13.2.0-qjohagx ninja/1.12.1-gcc-13.2.0-3zszrzs AdaptiveCpp/25.10.0 google-benchmark/1.9.4 googletest/1.17.0 kokkos/5.1.1 spdlog/1.16.0 VulkanSDK/1.4.350.0
