de49875dfc81d8ac056635315434c222d6954c85
Georg Eisner <georg.eisner0210@gmail.com>
2026-06-11 15:24:51 +0000

    add cuda test infrastructure

---
+++ test/nBodySimulation/cuda/direct_sum/CudaNBodyTest.cu
@@ -0,0 +1,12 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "NBodyTest.h"
+#include "nBodySimulation/cuda/direct_sum/Impl_Cuda.cuh"
+
+TEST_P(NBodyTest, ImplCuda_Implementation) {
+    const int size = GetParam();
+    this->runTest<ppb::ImplCuda<float>>(size);
+}
+
+INSTANTIATE_TEST_SUITE_P(BySize, NBodyTest, ::testing::Values(10, 100));
